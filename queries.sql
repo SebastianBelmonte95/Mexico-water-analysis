eff403f7bdcf4f6b0427c8c43f2196454e6dff6e
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2021-05-28 13:32:46 -0500

    Added water level comparison per dam by date

--- queries.sql
+++ queries.sql
@@ -1,5 +1,7 @@
+-- Importing the tablefunc module (PostgreSQL 9.1+)
+-- CREATE EXTENSION IF NOT EXISTS tablefunc;
 ----------------- 
---Creating table with dam name, location, capcity (name and namo), current storage, fill percentage and log date.
+--Creating table with id, dam name, location, capcity (name and namo), current storage, fill percentage and log date.
 -----------------
 -- CREATE TABLE dam_levels(
 -- 	dam_id VARCHAR(3) NOT NULL,
@@ -12,18 +14,28 @@
 -- 	log_date DATE
 -- )
 
--------- Importing data from CSV files into the DB --------
+--------- Importing data from a single CSV file into the DB ---------
 --  COPY dam_levels(dam_id,dam_name,state,name_storage,namo_storage,current_storage,fill_percent,log_date) 
 --  FROM 'D:\Google Drive\Professional Projects\Portfolio\Data Analysis\Water in Mexico\01 - Jan 2021\Jan01.csv' 
 --  DELIMITER ',' 
 --  CSV HEADER;
+
+------------------------ General Query ------------------------------
+-- SELECT * FROM dam_levels
+-- ORDER BY log_date
 
 -------- Total storage and Current Storage by state and date --------
 -- SELECT log_date,state,sum(namo_storage) as state_namo,sum(current_storage) as state_current FROM dam_levels
 -- GROUP BY state,log_date
 -- Order by state,log_date
 
-SELECT * FROM dam_levels
-WHERE dam_name='Chincua'
-ORDER BY log_date
+--Water level comparison per dam, Jan 1st vs May 27th --
+SELECT * FROM crosstab(
+	'SELECT dam_name,log_date,current_storage
+	FROM dam_levels
+	WHERE log_date = ''2021-01-01'' OR log_date = ''2021-05-27''
+	ORDER BY dam_id'
+)
+AS levels("Dam_name" VARCHAR(100), "Start" numeric, "End" numeric)
+
 
