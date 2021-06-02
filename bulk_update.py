0ddb729d8419d5b0ca0448347589e59e583cb98e
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2021-06-02 00:11:54 -0500

    Added truncate table and close connection

--- bulk_update.py
+++ bulk_update.py
@@ -6,6 +6,10 @@ conn = psycopg2.connect(
     "host=localhost dbname={} user=postgres password=password".format("Water_in_Mexico"))
 cur = conn.cursor()
 
+# Clearing all data from the table so it doesn't overlap
+cur.execute("TRUNCATE TABLE dam_levels")
+conn.commit()
+
 # Iterating all files for each folder in the current working directory
 
 for folder, sub_folders, files in os.walk(os.getcwd() + "\\Dam levels"):
@@ -17,3 +21,5 @@ for folder, sub_folders, files in os.walk(os.getcwd() + "\\Dam levels"):
             # Import data from CSV files to DB
             cur.copy_from(f, 'dam_levels', sep=',')
         conn.commit()
+cur.close()
+conn.close()
